001d20d43b3078540291a394a491eaa0e5f9b5cf
tobidelly <tobidelly@gmail.com>
2024-08-07 13:47:14 +0100

    README.md

---
+++ 100L_2nd-semester/cos_102_problem_solving/mid-semester_LAB/gpa.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Function to determine letter grade based on marks
+char determineGrade(int marks) {
+    if (marks >= 80 && marks <= 100) {
+        return 'A';
+    } else if (marks >= 70 && marks <= 79) {
+        return 'B';
+    } else if (marks >= 60 && marks <= 69) {
+        return 'C';
+    } else if (marks >= 50 && marks <= 59) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+int main() {
+    char course_code[100]; // Assuming the course code won't exceed 100 characters
+    int marks;
+
+    // Input course code and total marks from user
+    printf("Course: ");
+    scanf(" %[^\n]", course_code); // Read entire line including spaces
+
+    // Validate marks input
+    do {
+        printf("%s score: ", course_code);
+        scanf("%d", &marks);
+    } while (marks < 0 || marks > 100); // Loop until valid input is provided
+
+    // Determine and display the corresponding letter grade
+    char grade = determineGrade(marks);
+    printf("Course: %s, Grade: %c\n", course_code, grade);
+
+    return 0;
+}
--- 100L_2nd-semester/cos_112_web_technologies/Shopsy-miva/README.md
+++ 100L_2nd-semester/cos_112_web_technologies/Shopsy-miva/README.md
@@ -3,3 +3,10 @@
 
 Contributors:
 
+1. [Abubakar Abubakar](https://github.com/Azkiama-07)
+
+2. [Ahmad Major](https://github.com/Ahmad081255)
+
+3. [Tobi Delly](https://github.com/tobidelly)
+
+4. [Udemezue John](https://github.com/udemezue01)
